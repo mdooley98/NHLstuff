33a7eb910fd6f25a4c0184b30255b4ca6c1fd325
mdooley98 <mattjhdooley98@gmail.com>
2019-09-16 14:21:45 -0700

    Minor tweaks to formatting

--- Hockey/nhl_video_scraper.py
+++ Hockey/nhl_video_scraper.py
@@ -2,7 +2,7 @@ import requests, os, urllib.request, re
 from pathlib import Path
 base_dir = Path(__file__).parent
 
-class NHLVideoScraper():
+class NHLVideoScraper:
 
     def __init__(self):
         self.path = base_dir/'Videos'
@@ -15,7 +15,7 @@ class NHLVideoScraper():
         game_ids = []
 
         for game in response['dates']:
-            if game['games'][0]['status']['statusCode'] == '7': #Code 7 to exclude preseason games
+            if game['games'][0]['status']['statusCode'] == '7':  # Code 7 to exclude preseason games
                 game_ids.append(game['games'][0]['gamePk'])
 
         return game_ids
@@ -34,6 +34,7 @@ class NHLVideoScraper():
                 play['team']['name'] == 'Vancouver Canucks':
                     event_list.append(play['about']['eventId'])
 
+
             if event_list:
                 event_ids["gameId"] = game_id
                 event_ids["eventId"] = event_list
@@ -48,10 +49,10 @@ class NHLVideoScraper():
         for game in self.get_event_ids():
             url = 'https://statsapi.web.nhl.com/api/v1/game/{}/content'.format(game['gameId'])
             content = requests.get(url).json()
-            for id in game['eventId']:
+            for game_id in game['eventId']:
                 for play in content['media']['milestones']['items']:
                     try:
-                        if int(play['statsEventId']) == id:
+                        if int(play['statsEventId']) == game_id:
                             if play['highlight']['playbacks'][-1]['name'] == 'FLASH_1800K_960X540':
                                 description = re.sub(':', '', play['description'])
                                 date = re.findall(r'\d{4}-\d{2}-\d{2}', play['timeAbsolute'])
@@ -61,8 +62,8 @@ class NHLVideoScraper():
                                     continue
                                 urllib.request.urlretrieve(play['highlight']['playbacks'][-1]['url'], self.path/title)
                     except Exception as e:
-                        print(e)
                         pass
+
 
 x = NHLVideoScraper()
 x.download_videos()
